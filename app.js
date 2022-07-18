cb62ad0dc80042058ac1964d4f3202d340669612
maheshaila01 <maheshbabu09.amb@gmail.com>
2022-07-18 17:05:07 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes =require('./routes/blogRoutes');
 // express app
 const app = express();
 
@@ -32,55 +32,8 @@ app.get('/about', (req, res) => {
   res.render('about',{title : 'About'});
 });
 
-app.get('/blogs', (req, res) => {
-  // res.send('<p>home page</p>');
-  Blog.find().sort({createdAt: -1})
-  .then((result)=>{
-    res.render('index',{title : 'Welcome to Blog Guru',blogs:result});
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
-});
+app.use('/blogs',blogRoutes);
 
-app.post('/blogs',(req,res)=>{
-  const blog=new Blog(req.body);
-  blog.save()
-  .then((result)=>{
-    res.redirect('/blogs');
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
-});
-
-app.get('/blogs/create', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.render('create',{title : 'New Blog'});
-});
-
-app.get('/blogs/:id',(req,res)=>{
-  const id=req.params.id;
-  Blog.findById(id)
-   .then((result)=>{
-    res.render('details',{title: result.title,blog: result});
-   })
-   .catch((err)=>{
-    console.log(err);
-   })
-});
-
-app.delete('/blogs/:id',(req,res)=>{
-  const id=req.params.id;
-  Blog.findByIdAndDelete(id)
-  .then((result)=>{
-    res.json({redirect: '/blogs'});
-  })
-  .catch((err)=>{
-    console.log(err);
-  });
-});
-// 404 page
 app.use((req, res) => {
   res.status(404).render('404',{title: 'OOPS!'});
 });
